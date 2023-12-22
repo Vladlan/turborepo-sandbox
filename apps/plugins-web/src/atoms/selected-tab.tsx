f220b28669837a0b576b424c70b0ae3748ad5b18
Vladlan <lanvlad911@gmail.com>
2023-12-22 18:42:46 +0100

    Refactor code

--- apps/plugins-web/src/atoms/selected-tab.tsx
+++ apps/plugins-web/src/atoms/selected-tab.tsx
@@ -4,9 +4,8 @@ import getTabPlugins from "../api/get-tab-plugins";
 
 export const selectedTabAtom = atom<TabType | null>(null);
 
-
 export const selectedTabDataAtom = atom(async (get) => {
   const selectedTab = get(selectedTabAtom);
   if (!selectedTab) return [];
-  return getTabPlugins((selectedTab as TabType).id);
+  return getTabPlugins(selectedTab.id);
 });
--- apps/plugins-web/src/atoms/tabs.tsx
+++ apps/plugins-web/src/atoms/tabs.tsx
@@ -2,7 +2,7 @@ import { atom } from "jotai/vanilla";
 import getTabs from "../api/get-tabs";
 
 export const tabsData = atom(async () => {
-  return await getTabs()
-})
+  return await getTabs();
+});
 
 export const allPluginsDisabledAtom = atom<boolean>(false);
